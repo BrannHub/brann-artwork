9da4dc619be42ac6c97e5ea03bdf641addc3bf64
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-06-29 18:53:52 -0500

    Deleted useless files

--- debian/brann-artwork.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the brann-artwork package
-#
-0 4	* * *	root	[ -x /usr/bin/brann-artwork_maintenance ] && /usr/bin/brann-artwork_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(brann-artwork):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="brann-artwork" command="/usr/bin/brann-artwork"
